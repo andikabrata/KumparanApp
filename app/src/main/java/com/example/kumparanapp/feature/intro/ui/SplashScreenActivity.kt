354dbef39d7f9c1c536af0ad6ca5891fa323ba95
andikabrata <andikabrata06@gmail.com>
2022-03-25 10:26:30 +0700

    Add SplashScreen

---
+++ app/src/main/java/com/example/kumparanapp/feature/intro/ui/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.example.kumparanapp.feature.intro.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.example.kumparanapp.MainActivity
+import com.example.kumparanapp.core.base.BaseActivity
+import com.example.kumparanapp.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : BaseActivity<ActivitySplashScreenBinding>() {
+    override fun getViewBinding(): ActivitySplashScreenBinding {
+        return ActivitySplashScreenBinding.inflate(layoutInflater)
+    }
+
+    override fun onViewCreated(savedInstanceState: Bundle?) {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+
+}
